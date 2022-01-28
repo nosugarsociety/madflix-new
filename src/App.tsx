9c5d318596373e97244e14e1f923d12696b59da5
JU <>
2022-01-28 13:40:27 -0500

    Active route animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom';
 import React from 'react';
 import Header from './Components/Header';
 import Home from './Routes/Home';
 import Tv from './Routes/Tv';
 import Search from './Routes/Search';
 import './App.css';
+import {  } from 'react-router-dom';
 
 function App() {
   return (
